450d3244c2ab3b10acf15532b2ca719a5940d9a1
Subhajit Das <subhajit9004@gmail.com>
2025-05-23 23:24:47 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+//Print a welcome text in a separate line ...
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+
+cout << "Welcome to \n" << endl; 
+
+cout <<" w3resource.com " << endl;
+
+return 0;
+
+}
