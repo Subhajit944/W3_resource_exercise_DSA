9355b136b799bd94eb9cb0a2c1efdac7d0b28d10
Subhajit Das <subhajit9004@gmail.com>
2025-05-23 23:57:11 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+//Print the sum of two numbers 
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int a = 29;
+    int b = 30; 
+    int sum = a + b ;
+    cout << sum << endl;
+    return 0;
+}
+
