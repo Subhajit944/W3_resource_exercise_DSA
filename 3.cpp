951d983623d49a2943e6dc6a803ef7360bfd1abb
Subhajit Das <subhajit9004@gmail.com>
2025-05-24 21:31:22 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+//Find Size of fundamental data types 
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+
+   
+    cout << " The sizeof(char) is :              " << sizeof(char) << " Bytes \n" ;
+    cout << " The sizeof(int) is :               " << sizeof(int) << " Bytes \n" ;
+    cout << " The sizeof(float) is :             " << sizeof(float) << " Bytes \n" ;
+    cout << " The sizeof(bool) is :              " << sizeof(bool) << " bytes \n" ;
+    cout << " The sizeof(double) is :            " << sizeof(double) << " bytes \n" ;
+    cout << " The sizeof(long int) is :          " << sizeof(long int) << " bytes \n" ;
+    cout << " The sizeof(short int ) is :        " << sizeof(short int) << " bytes \n" ;
+    cout << " The sizeof(long long int ) is :    " << sizeof(long long int) << " bytes \n" ;
+
+
+    return 0;
+}
