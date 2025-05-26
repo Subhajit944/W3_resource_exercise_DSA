50b9058f1ad989c707feca0687e0d90bd0660bd2
Subhajit Das <subhajit9004@gmail.com>
2025-05-27 00:20:15 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,33 @@
+//Check whether the primitive values crossing the limits or not  
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    char gender = 'M';
+    bool isEmployed = true ;
+    unsigned short numOfsons = 1;
+    short yearOfAppt =2005;
+    unsigned int yearlyPackage =800000;
+    double height =79.48;
+    float gpa =9.12;
+    long totalDrawan =12047235L;
+    long long balance = 995324987LL;
+
+
+cout << " the Gender is  : "  <<gender <<endl;
+cout << " Is he married?  : " <<isEmployed <<endl;
+cout << " Number og sons he has : "<<numOfsons <<endl;
+cout << " Year of her appointment : "<< yearOfAppt <<endl;
+cout << " Salary for a year : "  << yearlyPackage << endl;
+cout << " Height is : "  << height << endl;
+cout << " GPA is : " << gpa << endl;
+cout << " Salary drawn up to : "  << totalDrawan << endl;
+cout << " Balance till : " << balance << endl;
+
+return 0;
+
+
+
+}
