da4e49989e0933cfd85d276c5d4f9a4e7bc851ac
Subhajit Das <subhajit9004@gmail.com>
2025-05-29 23:33:52 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+
+	int num1, num2, sum; 
+
+    cout << "Sum of two numbers :\n"; 
+
+	cout << "-------------------------\n"; 
+
+    cout << " Input 1st number : "; 
+
+	cin >> num1 ; 
+
+	cout << " Input 2nd number : "; 
+
+	cin >> num2; 
+
+	sum = num1 + num2; 
+
+	cout <<" The sum of the numbers is : " << sum << endl; 
+	
+	cout << endl;
+
+	return 0; 
+} 
+
