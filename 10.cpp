213d17ea2e83d944339de9b3a431850732e08248
Subhajit Das <subhajit9004@gmail.com>
2025-05-29 23:30:40 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{
+    cout << "\n\n Print the result of some specific operation :\n"; 
+	cout << "--------------------------------------------------\n"; 
+
+    cout << "Result of 1st expression is : "<< (-1+4*6) << endl; 
+    cout << "Result of 2nd expression is : "<< ((35+5)%7) <<endl; 
+    cout << "Result of 3rd expression is : "<< (14+-4*6/11) <<endl ; 
+    cout << "Result of 4th expression is : "<< (2+15/6*1-7%2) << endl;
+
+    return 0; 
+}
