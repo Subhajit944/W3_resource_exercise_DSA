98461ea863f26bf409722f403619db676c802951
Subhajit Das <subhajit9004@gmail.com>
2025-06-03 23:13:39 +0530

    Add files via upload

---
+++ 26.cpp
@@ -0,0 +1,31 @@
+//Find the area of Scalene Triangle....
+#include<iostream>
+#include <math.h>
+#define PI 3.14159
+using namespace std;
+
+int main(){
+    float side1 , side2 , ang1 , area ;
+    
+    cout << " Find the area of Scalene Triangle :\n";
+    cout<<"---------------------------------------\n";
+
+    cout << " Input the length of a side of the triangle : ";
+    cin >> side1;
+
+    cout << " Input the length of a side of the triangle : ";
+    cin >> side2;
+
+    cout << " Input the length of a side of the triangle : ";
+    cin >> ang1;
+
+    area =( side1 * side2 * sin((PI /180) * ang1)) /2;
+
+    cout << " The area of the Scalene Triangle is : " << area << endl;
+
+    cout << endl;
+
+    return 0;
+
+}
+
