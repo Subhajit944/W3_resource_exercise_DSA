8608ea310ee280fd970bda8d03188fc84d42dc9e
Subhajit Das <subhajit9004@gmail.com>
2025-06-05 23:54:55 +0530

    Add files via upload

---
+++ 30.cpp
@@ -0,0 +1,23 @@
+//Divide two numbers and print on the screen......
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+
+    cout << " Divide two numbers and print:\n"; 
+	cout << "----------------------------------\n"; 
+
+	int a;
+	int b; 
+	int resdiv; 
+
+	a = 30; 
+	b = 10; 
+
+	resdiv = a / b; 
+
+	cout << " The quotient of " << a << " and " << b << " is : " << resdiv << endl; 
+
+	return 0;
+}
