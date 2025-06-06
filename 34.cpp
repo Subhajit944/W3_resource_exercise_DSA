f0fc76082a5cdc22efb7d326fb1ae8c6c83189fa
Subhajit Das <subhajit9004@gmail.com>
2025-06-06 22:38:32 +0530

    Add files via upload

---
+++ 34.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+
+    cout << " Print the following pattern:\n"; 
+    cout << "--------------------------------\n";
+
+    cout << " xxxxx\n"; 
+    cout << "x     x       x        x\n";
+    cout << "x             x        x\n";
+    cout << "x          xxxxxxx  xxxxxxx\n";
+    cout << "x             x        x\n";
+    cout << "x     x       x        x\n";
+    cout << " xxxxx\n";
+
+    return 0; 
+}
