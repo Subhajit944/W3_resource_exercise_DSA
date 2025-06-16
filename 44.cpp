fbbc54d1d215941df0903d75846d08a3b00c16e1
Subhajit Das <subhajit9004@gmail.com>
2025-06-16 23:38:55 +0530

    Add files via upload

---
+++ 44.cpp
@@ -0,0 +1,25 @@
+//Enter length in centimeter and convert it into meter and kilometer....
+
+#include<iostream> 
+using namespace std; 
+
+int main() {
+
+    float km, met, cent; 
+
+    cout << " Convert centimeter into meter and kilometer :\n"; 
+    cout << "--------------------------------------------------\n";
+
+    cout << " Input the distance in centimeter : "; 
+    cin >> cent; 
+
+    met = (cent / 100); 
+    km = (cent / 100000); 
+
+    cout << " The distance in meter is: " << met << endl; 
+    cout << " The distance in kilometer is: " << km << endl; 
+
+    cout << endl; 
+
+    return 0; 
+}
