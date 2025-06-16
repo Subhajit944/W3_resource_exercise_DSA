9c4e5b6fc359a67fd0516d3a3969a801eab45894
Subhajit Das <subhajit9004@gmail.com>
2025-06-16 23:39:10 +0530

    Add files via upload

---
+++ 45.cpp
@@ -0,0 +1,23 @@
+//Converts kilometers per hour to miles per hour...
+
+#include<iostream> 
+using namespace std; 
+
+int main() {
+
+    float kmph, miph; 
+
+    cout << " Convert kilometers per hour to miles per hour :\n"; 
+    cout << "----------------------------------------------------\n";
+
+    cout << " Input the distance in kilometer : "; 
+    cin >> kmph; 
+
+    miph = (kmph * 0.6213712); 
+    
+    cout << " The " << kmph << " Km./hr. means " << miph << " Miles/hr." << endl;
+
+    cout << endl; 
+
+    return 0; 
+}
