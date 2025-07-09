29078b1cd4b5580acfce4fdeb3e6fde5046d5c8f
Subhajit Das <subhajit9004@gmail.com>
2025-07-09 22:45:35 +0530

    Add files via upload

---
+++ 70.cpp
@@ -0,0 +1,22 @@
+//Swap First and Last Characters...
+
+#include <iostream>
+using namespace std;
+
+string test(string str) {
+   
+    return str.length() > 1
+
+        ? str.substr(str.length() - 1) + str.substr(1, str.length() - 2) + str.substr(0, 1) 
+        : str; 
+}
+
+
+int main() {
+
+    cout << test("abcd") << endl;  
+    cout << test("a") << endl;     
+    cout << test("xy") << endl;    
+    
+    return 0;   
+}
